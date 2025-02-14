a9ddcaf906b80ca8c96556c11baa38ae13c5d641
extreme <bkhattri16@gmail.com>
2025-02-14 08:07:09 +0545

    added chat backend

--- src/index.js
+++ src/index.js
@@ -6,24 +6,28 @@ import bodyParser from "body-parser";
 import { errorHandler } from "./libs/errorhandler.js";
 import cors from "cors"
 import postRouter from "./routes/post.routes.js";
+import { createServer } from "http";
+import { socketHandler } from "./socket/socket.js";
 
 const app=express();
+const httpServer=createServer(app);
 const PORT= process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json())
+socketHandler(httpServer)
 app.get('/', (req, res)=>{
     res.status(statusCodes.OK).json({message:"Welcome to my app"})
 });
-
 
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
 
 
+
 app.use(errorHandler);
 
-app.listen(PORT, ()=>{
+httpServer.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`)
      
 });
